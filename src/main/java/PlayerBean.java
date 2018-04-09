91b84e718651d19a247ff3649802e409118a449e
pzdral220878 <pzdral220878@e-science.pl>
2018-04-09 21:33:20 +0200

    working on bean

---
+++ src/main/java/PlayerBean.java
@@ -0,0 +1,13 @@
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.io.Serializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlayerBean extends VBox implements Initializable, Serializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
