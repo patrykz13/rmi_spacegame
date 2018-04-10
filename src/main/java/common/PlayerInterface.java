2b7d7aa7ede0ee434bfeda5d4c12440f5713c328
pzdral220878 <pzdral220878@e-science.pl>
2018-04-11 01:17:09 +0200

    command

--- src/main/java/common/PlayerInterface.java
+++ src/main/java/common/PlayerInterface.java
@@ -8,5 +8,7 @@ public interface PlayerInterface extends Remote
     void confirmConnection(String message) throws RemoteException;
     void receiveCommand(SpaceCommand command) throws RemoteException;
     void receiveCommand(String command) throws RemoteException;
+    void becomeKickout(Boolean kickout) throws RemoteException;
+
 
 }
