464c4cd998d64c1adcff4648dc17a5c9e8b2382f
pzdral220878 <pzdral220878@e-science.pl>
2018-04-11 15:51:29 +0200

    change

--- src/main/java/common/ServerInterface.java
+++ src/main/java/common/ServerInterface.java
@@ -11,7 +11,7 @@ public interface ServerInterface extends Remote
     void removePlayer(String name) throws RemoteException;
     void removeCommander(String name) throws RemoteException;
     void broadcastScore(int score) throws RemoteException;
-    void broadcastNumberOfPlayers(int numberOfPlayers) throws RemoteException;
+    void broadcastNumberOfPlayers(int numberOfPlayers,String commanderName) throws RemoteException;
     void broadcastCommand(SpaceCommand command) throws RemoteException;
     void broadcastCommand(String type, String message) throws RemoteException;
     void broadcastCommand(String type, String message, String commanderName) throws RemoteException;
